15e7992312683a25f82dca08cf972b4aaa5e1e2c
himanshu-hims <gyananveshan@gmail.com>
2023-02-22 16:15:30 +0530

    this is second commit in testgit2.java file

---
+++ src/testpackage/TestGit2.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class TestGit2 {
+	public static void main(String [] args)
+	{
+		System.out.println("this is new junior test case ");
+		System.out.println("this is new junior test case ");
+		System.out.println("this is new junior test case ");
+		System.out.println("this is new junior test case ");
+	}
+
+}
